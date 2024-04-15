10ef6ad9e39aa391ffa5567596942aa7666d20aa
kimsjae <stwin755@gmail.com>
2024-04-15 16:49:21 +0900

    쇼핑카트 헤더 만들기 - setState() 함수

--- shoppingcart/lib/components/shoppingcart_header.dart
+++ shoppingcart/lib/components/shoppingcart_header.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shoppingcart/constants.dart';
 
 class ShoppingCartHeader extends StatefulWidget {
+  const ShoppingCartHeader({Key? key}) : super(key: key);
+
   @override
-  _ShoppingCartHeaderState createState() => _ShoppingCartHeaderState();
+  State<ShoppingCartHeader> createState() => _ShoppingCartHeaderState();
 }
 
 class _ShoppingCartHeaderState extends State<ShoppingCartHeader> {
@@ -17,6 +21,59 @@ class _ShoppingCartHeaderState extends State<ShoppingCartHeader> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return Column(
+      children: [
+        _buildHeaderPic(),
+        _buildHeaderSelector(),
+      ],
+    );
+  }
+
+  Widget _buildHeaderPic() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: AspectRatio(
+        aspectRatio: 5 / 3,
+        child: Image.asset(
+          selectedPic[selectedId],
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeaderSelector() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildHeaderSelectorButton(0, Icons.directions_bike),
+          _buildHeaderSelectorButton(1, Icons.motorcycle),
+          _buildHeaderSelectorButton(2, CupertinoIcons.car_detailed),
+          _buildHeaderSelectorButton(3, CupertinoIcons.airplane),
+        ],
+      ),
+    );
+  }
+
+  // 1. 다른 화면에서도 재사용하면 공통 컴포넌트 위젯으로 관리하는 것이 좋다.
+  Widget _buildHeaderSelectorButton(int id, IconData mIcon) {
+    return Container(
+      width: 70,
+      height: 70,
+      decoration: BoxDecoration(
+        color: id == selectedId ? kAccentColor : kSecondaryColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: IconButton(
+        icon: Icon(mIcon, color: Colors.black),
+        onPressed: () {
+          setState(() {
+            selectedId = id;
+          });
+        },
+      ),
+    );
   }
 }
