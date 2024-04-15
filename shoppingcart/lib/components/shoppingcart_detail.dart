2d375c33ed53819ed4e4822b61062365a1444700
kimsjae <stwin755@gmail.com>
2024-04-15 16:42:06 +0900

    뼈대 만들기

---
+++ shoppingcart/lib/components/shoppingcart_detail.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ShoppingCartDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox();
+  }
+}
---
+++ shoppingcart/lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shoppingcart/components/shoppingcart_detail.dart';
+import 'package:shoppingcart/components/shoppingcart_header.dart';
+import 'package:shoppingcart/theme.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: theme(),
+      home: ShoppingCartPage(),
+    );
+  }
+}
+
+class ShoppingCartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildShoppingCartAppBar(),
+      body: Column(
+        children: [
+          ShoppingCartHeader(),
+          ShoppingCartDetail(),
+        ],
+      ),
+    );
+  }
+
+  AppBar _buildShoppingCartAppBar() {
+    return AppBar();
+  }
+}
